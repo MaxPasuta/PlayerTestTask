4d7ba460319abe66fdb69b28557d64ae101c26a1
MaxPasuta <pasutmaks@gmail.com>
2022-04-06 09:28:24 +0700

    Initial Commit

---
+++ PlayerTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlayerTestTask
+//
+//  Created by Максим Пасюта on 06.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
